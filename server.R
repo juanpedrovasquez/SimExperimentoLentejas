18cdb6f36e84b23e79d68505b9a440ef34e51ab8
juanpedrovasquez <juanpedrovasquez@gmail.com>
2014-06-26 09:52:30 -0500

    ver 0

---
+++ server.R
@@ -0,0 +1,44 @@
+library(shiny)
+library(ggplot2)
+library(splines)
+
+
+shinyServer(function(input, output) {
+  
+  
+  
+  output$distPlot <- renderPlot({
+    set.seed(19)
+    poblac <- input$poblac
+    factorCrecim <- input$factorCrecim
+    numTiradas <- input$numTiradas    
+    tiradaS <<- data.frame(matrix(NaN,numTiradas,1),matrix(NaN,numTiradas,1))
+    names(tiradaS) <<- c('Tirada','Lentejas')
+    for (i in 1:numTiradas){
+      t <- floor(runif(poblac,1,64))
+      blancos <- t[t%%2==0]
+      tiradaS[i,1] <<- i
+      tiradaS[i,2] <<- poblac
+      poblac <- round(factorCrecim * length(blancos),0)
+    }
+    fit <- lm(log(tiradaS$Lentejas) ~ tiradaS$Tirada )
+    M0 <- round(fit$coefficients[[2]],2)
+    A0 <- round(exp(fit$coefficients[[1]]),2)
+    R2 <- round(summary(fit)$r.squared,3)
+    
+    g <- qplot(x=tiradaS$Tirada,y=tiradaS$Lentejas, geom=c('point','smooth'),main=paste('Lentejas =',A0,'exp(',M0,' * Tirada);  R²=',R2),ylab='Lentejas en cuadro blanco', xlab='Número de Tirada')  
+    g
+    
+  })
+
+  
+output$datos <- renderTable({
+  set.seed(19)
+  poblac <- input$poblac
+  factorCrecim <- input$factorCrecim
+  numTiradas <- input$numTiradas    
+  tiradaS
+})
+
+
+})
---
+++ ui.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Simulador Experimento Lentejas"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("poblac",
+                  "Núm. inicial de lentejas:",
+                  min = 3,
+                  max = 12,
+                  value = 7),
+      sliderInput("factorCrecim",
+                  "Factor de crecimiento:",
+                  min = 3,
+                  max = 6,
+                  value = 3),
+      sliderInput("numTiradas",
+                  "Total de tiradas:",
+                  min = 5,
+                  max = 20,
+                  value = 10)
+      
+    ),
+    
+    
+      
+    
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      h2("Tabla de datos", align='center' ),
+      HTML('<center>'),
+      tableOutput("datos"),
+      HTML('</center>')
+    )
+  )
+))
